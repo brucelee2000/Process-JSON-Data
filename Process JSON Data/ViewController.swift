4c56bb8b313f484f590cb2bf84c1714b9b0db202
brucelee2000 <brucelee2000@tp.edu.tw>
2015-01-24 22:20:04 -0800

    Process JSON by NSJSONSerialization class

--- Process JSON Data/ViewController.swift
+++ Process JSON Data/ViewController.swift
@@ -13,6 +13,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlPath = "http://www.telize.com/geoip"
+        var url = NSURL(string: urlPath)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithURL(url!, completionHandler: { (webData, webResponse, webError) -> Void in
+            if (webError != nil) {
+                println(webError)
+            } else {
+                var jsonError:NSError?
+                // NSJSONSerialization class to convert JSON to Foundation objects and convert Foundation objects to JSON
+                let jsonResult = NSJSONSerialization.JSONObjectWithData(webData, options: NSJSONReadingOptions.MutableContainers, error: &jsonError) as NSDictionary
+                
+                println(webData)
+                println(jsonResult)
+                println(jsonResult["ip"]!)
+            }
+        })
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
